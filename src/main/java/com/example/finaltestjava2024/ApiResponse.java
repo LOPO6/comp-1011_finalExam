c882f39483f54adc57799c87b090b1070060d668
Logan <lbpotopn@lakeheadu.ca>
2024-12-11 19:06:14 -0500

    Question 2 complete

--- src/main/java/com/example/finaltestjava2024/ApiResponse.java
+++ src/main/java/com/example/finaltestjava2024/ApiResponse.java
@@ -1,5 +1,11 @@
 package com.example.finaltestjava2024;
 
+import java.util.List;
 public class ApiResponse {
+    private List<customer> Company;
+
+    public List<customer> getCompany(){
+        return Company;
+    }
 
 }
